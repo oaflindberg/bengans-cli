d17f0d13bcd48ff0698294606d37f4976ce98a6c
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2021-09-01 10:53:16 +0200

    WIP

--- src/views/exit.ts
+++ src/views/exit.ts
@@ -1,6 +1,7 @@
 import chalk from 'chalk';
+import puppeteer from 'puppeteer';
 
-export const exit = async (browser) => {
+export const exit = async (browser: puppeteer.Browser) => {
 	browser.close();
 	console.log(chalk.yellowBright('Bye 👋'));
 };
